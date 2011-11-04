f338ed6a2b8938aa8d4514ed2c57218051e3078e
Shane Preece <shamess@gmail.com>
2011-11-04 00:50:47 +0000

    Outputs a TOC now!

--- jquery.tableofcontents.js
+++ jquery.tableofcontents.js
@@ -3,11 +3,9 @@
 
 	var settings = {
 		// only look for H# tags inside the scope
-		scope: me ( 'body' ),
-		// where the table will be drawn
-		table_div: '',
+		scope: $( 'body' ),
 		// the only tags we'll make headers from
-		header_tags: [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ];
+		header_tags: [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ],
 		// will the list be a UL, or OR?
 		list_type: 'ul'
 	};
@@ -16,14 +14,26 @@
 		init : function ( options ) {
 			// merge passed settings with our own
 			if ( options ) $.extend( settings, options );
+			
+			// start on the contents bit
+			table = $("<" + settings.list_type + "></" + settings.list_type + ">");
+			
+			// get all the tags we're looking for
+			$(settings.header_tags.join (', ')).each (function () {
+				table.append ('<li>' + $(this).html() + '</li>');
+			});
+			
+			$(this).append (table);
+			
+			return $(this);
 		}
 	};
 	
-	$.fn.tableofcontents = function () {
+	$.fn.tableofcontents = function (method) {
 		// Method calling logic
 		if ( methods[method] ) {
 			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-		} else if ( typeof method === 'object' || ! method ) {
+		} else if ( typeof method === 'object' || undefined == method ) {
 			return methods.init.apply( this, arguments );
 		} else {
 			$.error( 'Method ' +  method + ' does not exist on jQuery.tableofcontents' );
